6719ae741d90f66708168ca40cc1b1c21ef3eb22
Shane Chang <kxykxyou@gmail.com>
2023-02-26 18:36:48 +0800

    feat: build connection to open ai
    with text-davinci-003, max_tokens = 2048

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+import os, openai
 
-app = Flask(__name__)
+openai.api_key: str = os.getenv("OPENAI_ACCESS_KEY")
+CHATBOT_API: str = "https://api.openai.com/v1/engines/davinci-codex/completions"
+
+app: Flask = Flask(__name__)
 
 
-@app.route("/hello")
-def hello():
-    return "Hello, World!"
+@app.route("/chatbot", methods=["POST"])
+def chatbot() -> str:
+    response = openai.Completion.create(
+        model="text-davinci-003", prompt=request.json["message"], max_tokens=2048
+    )
+    print("request: ", request)
+    print("type: ", type(response))
+
+    return response.choices[0].text
 
 
 if __name__ == "__main__":
